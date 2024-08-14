5289d00d22b388ebd7e12b540a789d47a6be855d
Sébastien Hinderer <Sebastien.Hinderer@ens-lyon.org>
2024-08-14 19:34:31 +0200

    Working HTTPS requst to OverPass using eIO

--- bin/main.ml
+++ bin/main.ml
@@ -4,24 +4,48 @@
 
 let _base_url = "https://api.notion.com/"
 
-let _wget host path headers =
-  Eio_main.run @@ fun env ->
+let https ~authenticator =
+  let tls_config = Tls.Config.client ~authenticator () in
+  fun uri raw ->
+  let host =
+    (* Domain_name.host_exn (Domain_name.of_string "overpass-api.de") *)
+      Uri.host uri
+      |> Option.map (fun x -> Domain_name.(host_exn (of_string_exn x)))
+    in
+    Tls_eio.client_of_flow ?host tls_config raw
+
+let overpass_request =
+  String.concat "\n"
+  [
+    "[out:json];";
+    "node";
+    "  [amenity~\"restaurant|fast_food|cafe\"]";
+    "  (48.8412188, 2.3485422, 48.8444763, 2.3536409);";
+    "out;"
+  ]                                      
+
+let wget env sw host path headers body =
   Easy_tls.run env @@ fun tls ->
   Eio.Net.with_tcp_connect ~host ~service:"https" env#net @@ fun connection ->
   let connection =
     Easy_tls.client_of_flow tls ~host connection 
   in
-  let (http_response, reader) =
-    Cohttp_eio.Client.get ~headers ~conn:connection (host, None) path
+  let null_auth ?ip:_ ~host:_ _ = Ok None in
+  let client =
+    Cohttp_eio.Client.make ~https:(Some (https ~authenticator:null_auth)) env#net
+  in
+  let (http_response, body) =
+      Cohttp_eio.Client.post client ~sw ~headers ~body path
   in      
   Eio.Flow.shutdown connection `Send;
   match Http.Response.status http_response with
-    | `OK ->
-      Ok (Cohttp_eio.Client.read_fixed (http_response, reader))
+  | `OK ->
+    Ok
+      (Eio.Buf_read.(parse_exn take_all) body ~max_size:max_int)
     | status -> Error 
-      ((Http.Status.to_string status) ^ (Eio.Buf_read.take_all reader))
+      ((Http.Status.to_string status) ^ (Eio.Buf_read.(parse_exn take_all) body ~max_size:max_int))
 
-let wget_notion id =
+let _wget_notion id =
   let path = "https://api.notion.com/v1/blocks/" ^ id ^ "/children" in
   let token = Sys.getenv "NOTION_TOKEN" in
   let headers = (* Http.Header.of_list *)
@@ -41,9 +65,15 @@ let wget_notion id =
   | _ -> Error "ezcurl internal error"
 
 let _ =
-  match (wget_notion "49970e75e46e49b89d176926940b1acc") with
-  | Ok s ->
-    let json_object = Yojson.Safe.from_string s in
+  let path = Uri.of_string "https://overpass-api.de/api/interpreter" in
+  let headers = Http.Header.of_list [] in
+  let body = Cohttp_eio.Body.of_string overpass_request in
+  Eio_main.run @@ fun env ->
+  Eio.Switch.run @@ fun sw ->
+  match (wget env sw "overpass-api.de" path headers body) with
+  | Ok s -> print_endline s
+                          (*
+                            let json_object = Yojson.Safe.from_string s in
     let open Yojson.Safe.Util in
     let string_member key obj = member key obj |> to_string in
     let results = member "results" json_object |> to_list in
@@ -54,4 +84,5 @@ let _ =
     in
     let subpages = List.filter p results in
     List.iter (fun block -> print_endline (string_member "id" block)) subpages
+*)
   | Error e -> print_string e
